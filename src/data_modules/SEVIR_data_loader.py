8226a8ada2872704bf204b27f2bbefb2a4623fa2
FiedorPW <mikolajfiedorczuk@gmail.com>
2025-01-04 14:56:39 +0100

    feat: files loaded from directory automaticly, percentage split test,train,val as a parameter

--- src/data_modules/SEVIR_data_loader.py
+++ src/data_modules/SEVIR_data_loader.py
@@ -10,48 +10,23 @@ from data_exploration import visualize_batch_tensor_interactive
 import os
 os.environ["HDF5_USE_FILE_LOCKING"]='FALSE'
 
-
 TIME_STEPS = 49
-
-all_file_paths_2019 = [
-    "../../data/2019/SEVIR_IR069_RANDOMEVENTS_2019_0101_0430.h5", #val
-    "../../data/2019/SEVIR_IR069_RANDOMEVENTS_2019_0501_0831.h5", #tesut
-    "../../data/2019/SEVIR_IR069_RANDOMEVENTS_2019_0901_1231.h5", #train
-    "../../data/2019/SEVIR_IR069_STORMEVENTS_2019_0101_0630.h5", # val
-    "../../data/2019/SEVIR_IR069_STORMEVENTS_2019_0701_1231.h5"  #test
-]
-all_file_paths_2018 = [ # train
-    "../../data/2018/SEVIR_IR069_RANDOMEVENTS_2018_0101_0430.h5",
-    "../../data/2018/SEVIR_IR069_RANDOMEVENTS_2018_0501_0831.h5",
-    "../../data/2018/SEVIR_IR069_RANDOMEVENTS_2018_0901_1231.h5",
-    "../../data/2018/SEVIR_IR069_STORMEVENTS_2018_0701_1231.h5",
-    "../../data/2018/SEVIR_IR069_STORMEVENTS_2018_0101_0630.h5"
-]
-all_file_paths = all_file_paths_2018 + all_file_paths_2019
-
-#test val train split(each includes at least one storm file)
-train_files = all_file_paths_2018 + [all_file_paths_2019[2]]
-
-validate_files = [all_file_paths_2019[0],all_file_paths_2019[3]]
-
-test_files = [ all_file_paths_2019[1],all_file_paths_2019[4]]
-
 
 class SEVIR_dataset(Dataset):
     """
     Dataset ładujący dane SEVIR z wielu plików HDF5.
     """
-    def __init__(self, file_paths, step, width, height, sequence_length):
+    def __init__(self, samples_dir_path, step, width, height, sequence_length):
         super().__init__()
-        # Convert relative paths to absolute paths
-        self.file_paths = [os.path.abspath(path) for path in file_paths]
-
+        # crawl directory to retrive paths to h5 files absolute paths
+        self.file_paths = self._get_h5_files(samples_dir_path)
         self.samples_per_file = []
         self._cumulative_indices = []
         self.step = step
         self.width = width
         self.height = height
         self.sequence_length = sequence_length
+
 
         current_cum = 0
         for path in self.file_paths:
@@ -105,7 +80,7 @@ class SEVIR_dataset(Dataset):
                 # check czy oczekiwana długość jest mniejsza niż wzięcie co ntej klatki, a torch robi ceil przy samplowaniu
                 if self.sequence_length <= math.ceil(TIME_STEPS/self.step):
                     permuted_sample_step_len = permuted_sample_step[:self.sequence_length]
-                # zmiana rozmiaru na np. 25 x height x width
+                # zmiana rozmiaru na na np. 25 x height x width
                 if self.width != 192 or self.height != 192:
                     resize = transforms.Resize((self.height, self.width), antialias=True)
                     permuted_sample_step_resized = resize(permuted_sample_step_len)
@@ -120,6 +95,18 @@ class SEVIR_dataset(Dataset):
         except Exception as e:
             print(f"Error loading file {file_path} at index {local_index}")
             raise e
+
+    def _get_h5_files(self, directory):
+        """
+        Recursively finds all .h5 files in the given directory and its subdirectories.
+        Returns list: A list of absolute paths to .h5 files.
+        """
+        h5_files = []
+        for root, _, files in os.walk(directory):
+            for file in files:
+                if file.endswith('.h5'):  # Check for .h5 extension
+                    h5_files.append(os.path.abspath(os.path.join(root, file)))
+        return h5_files
 
     def _find_file_index(self, index):
         for i, cum in enumerate(self._cumulative_indices):
@@ -152,25 +139,25 @@ class ConvLSTMSevirDataModule(pl.LightningDataModule):
         height,
         sequence_length,
         # przypisanie plików do zbiorów
-        train_files=train_files,
-        val_files=validate_files,
-        test_files=test_files,
-        file_paths=all_file_paths,
+        files_dir,
         # parametry DataLoadera
         batch_size=4,
-        num_workers=2
+        num_workers=2,
+        train_files_percent=70,
+        val_files_percent=15,
+        test_files_percent=15
     ):
         super().__init__()
-        self.train_files = train_files
-        self.val_files = val_files
-        self.test_files = test_files
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.file_paths = file_paths
+        self.files_dir = files_dir
         self.step = step
         self.width = width
         self.height = height
         self.sequence_length = sequence_length
+        self.train_files_percent = train_files_percent
+        self.val_files_percent = val_files_percent
+        self.test_files_percent = test_files_percent
 
         self.train_dataset = None
         self.val_dataset = None
@@ -184,12 +171,22 @@ class ConvLSTMSevirDataModule(pl.LightningDataModule):
 
     def setup(self, stage=None):
         # Tworzymy dataset-y.
+        full_dataset = SEVIR_dataset(
+            self.files_dir,
+            self.step,
+            self.width,
+            self.height,
+            self.sequence_length)
+        train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(
+            full_dataset,
+            [self.train_files_percent, self.val_files_percent, self.test_files_percent])
+
         if stage == 'fit' or stage is None:
-            self.train_dataset = SEVIR_dataset(self.train_files, self.step, self.width, self.height, self.sequence_length)
-            self.val_dataset   = SEVIR_dataset(self.val_files, self.step, self.width, self.height, self.sequence_length)
+            self.train_dataset = train_dataset
+            self.val_dataset   = val_dataset
 
         if stage == 'test' or stage is None:
-            self.test_dataset  = SEVIR_dataset(self.test_files, self.step, self.width, self.height, self.sequence_length)
+            self.test_dataset  = test_dataset
 
     def train_dataloader(self):
         return DataLoader(
@@ -221,18 +218,38 @@ if __name__ == "__main__":
     # tylko jeden jest na pytorch a drugi na pytorch lighnting
 
     ''' pytorch dataset '''
+    file_path_h5_dir = "../../data/"
     # przyjmuje step oraz szerokość i wysokość obrazka, oraz długość sekwencji(ucinamy 2 klatki tutaj)
-    dataset = SEVIR_dataset(train_files, 3, 128, 128, 15)
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=1)
+    full_dataset = SEVIR_dataset(file_path_h5_dir, 3, 128, 128, 15)
+    # podział na zbiory, procentowy
+    train_dataset,  val_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [0.7, 0.15, 0.15])
+    # przykładowy dataloader dla train datasetu
+    dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=1)
     fist_sample = next(iter(dataloader))
     print("data loader",fist_sample.shape) # zwraca torch.Size([10, 17, 128, 128])
+    print("sample split data loader",len(train_dataset),len(val_dataset),len(test_dataset),"\n")
     # visualize_batch_tensor_interactive(fist_sample, 0, "SEVIR dataset")
 
     ''' pytorch lightning datamodule '''
-    # przykład użycia
-    dm = ConvLSTMSevirDataModule(step=3, width=128, height=128, batch_size=4, num_workers=1, sequence_length=15)
+    # # przykład użycia
+    dm = ConvLSTMSevirDataModule(
+        step=3,
+        width=128,
+        height=128,
+        batch_size=4,
+        num_workers=1,
+        sequence_length=15,
+        train_files_percent=0.7,
+        val_files_percent=0.15,
+        test_files_percent=0.15,
+        files_dir=file_path_h5_dir)
+
     dm.setup('fit')
     train_loader = dm.train_dataloader()
+    dm.setup('test')
+    test_loader = dm.test_dataloader
+    val_loader = dm.val_dataloader
     batch = next(iter(train_loader))
     print("data module",batch.shape) # zwraca torch.Size([4, 17, 128, 128
+    print("sample split data loader",len(dm.train_dataset),len(dm.val_dataset),len(dm.test_dataset))
     # visualize_batch_tensor_interactive(batch, 0, "SEVIR dataset")
